0736a36ac8f304864168063d163518f6e200fc08
kiera <kd.hockey12@gmail.com>
2025-10-31 11:58:31 -0600

    Edited sql queries

--- source.sql
+++ source.sql
@@ -3,29 +3,42 @@ CREATE DATABASE dbassign2;
 USE dbassign2;
 
 CREATE TABLE deptInfo (
-	id INT PRIMARY KEY,
-	DOE DATE,
+	deptID TEXT PRIMARY KEY,
+	deptDOE DATE,
 	deptName TEXT
 	);
 
+CREATE TABLE employInfo(
+	employID TEXT PRIMARY KEY,
+	employDOB DATE,
+	employHireDate DATE,
+	employDept TEXT
+);
+
 CREATE TABLE studentCounselling (
-	id INT PRIMARY KEY,
-	DOA DATE,
-	DOB DATE,
-	deptChoices INT,
-	deptAdmission INT
+	stuID TEXT PRIMARY KEY,
+	stuDOA DATE,
+	stuDOB DATE,
+	stuDeptChoices TEXT,
+	stuDeptAdmission TEXT
 	);
 
 CREATE TABLE studentPerformance (
-	infile INT PRIMARY KEY,
-	semester INT,
-	paperId INT,
+	stuID TEXT PRIMARY KEY,
+	semester TEXT,
+	paperID TEXT,
 	paperName TEXT,
 	marks INT,
 	effortHours INT
 	);
+
+
 LOAD DATA LOCAL INFILE '/data/Department_Information.csv' 
 INTO TABLE deptInfo 
+FIELDS TERMINATED BY ',' IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE '/data/Employee_Information.csv' 
+INTO TABLE employInfo 
 FIELDS TERMINATED BY ',' IGNORE 1 LINES;
 
 LOAD DATA LOCAL INFILE '/data/Student_Counceling_Information.csv' 
